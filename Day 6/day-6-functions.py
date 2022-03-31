05f6a2dd7eb04532575ab047bef7079ec6b01489
Ryan Davis <79614080+returnRyan@users.noreply.github.com>
2022-03-30 21:38:47 -0400

    Add files via upload

---
+++ Day 6/day-6-functions.py
@@ -0,0 +1,28 @@
+# print("Hello")
+# num_char = len("Hello")
+# print(num_char)
+
+def my_function(): # def defines the function, called my_function(), followed by :
+  print("Hello")
+  print("Bye")
+
+my_function() # calls my_function()
+
+# Reeborg's World
+def turn_around():
+    turn_left()
+    turn_left()
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+# Draw Square using defined functions
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
